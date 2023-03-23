fa1b8b4c39916351c41797c4fd0b2b3329871ce7
Damian Danielczyk <damian.danielczyk@swing.dev>
2023-03-23 19:03:04 +0100

    Fix inject script

--- Scripts/inject.sh
+++ Scripts/inject.sh
@@ -3,7 +3,7 @@
 SCRIPTPATH=$(dirname "$0")
 TEMPLATES=${TEMPLATES:-"$SCRIPTPATH/../Templates"}
 
-if [ -z "SOURCERY_BINPATH" ]; then
+if [ -n "SOURCERY_BINPATH" ]; then
   SOURCERY_BINPATH="$SOURCERY_BINPATH"
 elif [ -f "$PODS_ROOT/Sourcery/bin/sourcery" ]; then
   SOURCERY_BINPATH="$PODS_ROOT/Sourcery/bin/sourcery"
@@ -11,16 +11,16 @@ else
   SOURCERY_BINPATH="sourcery"
 fi
 
-if [ -z "INJECT_OUTPUT" ]; then
-  INJECT_OUTPUT="$SRCROOT/Generated/"
-else
+if [ -n "INJECT_OUTPUT" ]; then
   INJECT_OUTPUT="$INJECT_OUTPUT"
+else
+  INJECT_OUTPUT="$SRCROOT/Generated/"
 fi
 
-if [ -z "INJECT_INPUT" ]; then
-  INJECT_INPUT="$SRCROOT"
-else
+if [ -n "INJECT_INPUT" ]; then
   INJECT_INPUT="$INJECT_INPUT"
+else
+  INJECT_INPUT="$SRCROOT"
 fi
 
 set -x
